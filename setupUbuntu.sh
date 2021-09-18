70732421b18e7d36800fc3bfd3acc323c0e17621
christopherrons <christopherheron09@gmail.com>
2021-09-18 20:23:12 +0200

    Added ubuntu setup file

---
+++ setupUbuntu.sh
@@ -0,0 +1,26 @@
+# Update
+sudo apt-get update
+
+# Install Z shell, Oh My z Shell + plugins
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+# Add to zshrc file
+#plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+#ZSH_THEME="af-magic"
+
+# Install Java, Maven and Python
+sudo apt-get install openjdk-11-jdk
+sudo apt-get install maven
+
+# Install Intellij and Pycharm
+sudo snap install intellij-idea-community --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install pycharm-idea-community --classic
+
+# Install Guake
+sudo apt-get install guake
+
+# Install Spotify
+sudo snap install spotify
